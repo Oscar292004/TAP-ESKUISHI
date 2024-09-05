30db34bc451f4411d7deecef2f12e9c1dbfc7358
Ines <martinezines387@gmail.com>
2024-09-04 21:16:34 -0600

    Modificacion Ines

--- HolaMundo.java
+++ HolaMundo.java
@@ -5,6 +5,10 @@ public class HolaMundo {
 	System.out.println("Prueba de Segunda Edición"):
 	System.out.println("Prueba de Tercera Edición");
 	System.out.println("Hola mundo. Edicion Valeria");
+	System.out.println("Holi mundo. Edicion Ines");
+	System.out.println("Segundo intento- Ines");
+	System.out.println("La tercera es la buena-Ines");
+	System.out.println("Hola de nuevo- Ines");
 
     }
 }
